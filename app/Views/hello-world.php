78679c5ddbce5609160f784c4e1eeeb24f7169d1
fauzan <fauzanalghifari963@gmail.com>
2023-11-07 21:31:14 +0700

    finish

---
+++ app/Views/hello-world.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+    <style>
+        body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            font-size: 24px;
+            color: #333;
+            text-align: center;
+            margin-top: 100px;
+        }
+        h1 {
+            font-size: 72px;
+            color: #ff6600;
+            margin-bottom: 50px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Hello World!</h1>
+    <p>Welcome to my awesome PHP page!</p>
+    <p>Ahmad Fauzan Alghifari - 5025211091</p>
+</body>
+</html>
